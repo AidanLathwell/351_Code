9b0e60e360b8a679e3b090225f72129f4dd9c85f
Aidan Lathwell <ajlathwell@icloud.com>
2025-09-01 18:37:14 -0400

    Homework One

---
+++ ComparatorTwoBitTestWeak.java
@@ -0,0 +1,31 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static edu.gvsu.dlunit.DLUnit.*;
+
+public class ComparatorTwoBitStrong {
+@Test
+public void zero_zero() {
+	for(int a=0, a <=3, a++){
+		for(int b=0, b<=3, b++){	
+		  
+			  
+	  
+    		setPinUnsigned("InputA", a);
+   		setPinUnsigned("InputB", b);
+		
+		run();
+		int expected;
+
+		if (a < b){
+			expected = 1;
+		}
+		else{
+			expected = 0;
+
+		}
+
+    		Assert.assertEquals(expected, readPinUnsigned("Output"));
+  }
+
+}
