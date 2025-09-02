7b038c49c4f04339e3474a493b288326bdf0d783
AidanLathwell <160864640+AidanLathwell@users.noreply.github.com>
2025-09-01 20:44:28 -0400

    Update ComparatorTwoBitTestWeak.java

--- ComparatorTwoBitTestWeak.java
+++ ComparatorTwoBitTestWeak.java
@@ -6,8 +6,8 @@ import static edu.gvsu.dlunit.DLUnit.*;
 public class ComparatorTwoBitStrong {
 @Test
 public void zero_zero() {
-	for(int a=0, a <=3, a++){
-		for(int b=0, b<=3, b++){	
+	for(int a=0; a <=3; a++){
+		for(int b=0; b<=3; b++){	
 		  
 			  
 	  
